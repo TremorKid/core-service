cdb7e50ac338420a756c07addd7552ca8858fb10
EcoCharlie <erikc.cortezb@gmail.com>
2024-06-14 07:11:03 -0500

    developer: Controller General Parameter Updated

--- src/main/java/com/tremorkid/coreservice/app/general/controller/GeneralParameterController.java
+++ src/main/java/com/tremorkid/coreservice/app/general/controller/GeneralParameterController.java
@@ -1,6 +1,5 @@
 package com.tremorkid.coreservice.app.general.controller;
 
-import com.tremorkid.coreservice.app.general.beans.GeneralParameterBean;
 import com.tremorkid.coreservice.app.general.service.GeneralParameterService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @Log4j2
 @Api
 @RestController
-@RequestMapping("/service/generalParameter")
+@RequestMapping("/service/general-parameter")
 @Tag(name = "GeneralParameterController", description = "GeneralParameter EndPoint")
 public class GeneralParameterController {
 
@@ -23,7 +22,7 @@ public class GeneralParameterController {
     @GetMapping("/{code}")
     public ResponseEntity<?> getGeneralParameter(@PathVariable("code") String code) {
         try {
-            return new ResponseEntity<>(generalParameterService.getByCode(code), HttpStatus.OK);
+            return new ResponseEntity<>(generalParameterService.getByCode(code).getValue(), HttpStatus.OK);
         } catch (Exception e) {
             log.error("Error: ", e);
             return new ResponseEntity<>("Error", HttpStatus.INTERNAL_SERVER_ERROR);
